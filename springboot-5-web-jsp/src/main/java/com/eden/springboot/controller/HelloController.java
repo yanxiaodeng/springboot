9ad6ad0dc3fc3680e7219ebbf677474ea8d1f8b5
yanxiaodeng <1542280510@qq.com>
2019-12-24 19:49:22 +0800

    brabch test

--- springboot-5-web-jsp/src/main/java/com/eden/springboot/controller/HelloController.java
+++ springboot-5-web-jsp/src/main/java/com/eden/springboot/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+	//测试的controller
     @GetMapping("/hello")
     public String hello(Model model)
     {

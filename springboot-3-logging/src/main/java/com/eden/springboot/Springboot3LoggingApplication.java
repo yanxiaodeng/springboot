e958e80c4b79f092780df7f6b5b60734f14c3496
yanxiaodeng <1542280510@qq.com>
2019-12-24 12:49:10 +0800

    springboot-03-config

---
+++ springboot-3-logging/src/main/java/com/eden/springboot/Springboot3LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.eden.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3LoggingApplication.class, args);
+    }
+
+}

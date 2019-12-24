74640636262d23f1e2ab8025c7693b04b846a3e7
yanxiaodeng <1542280510@qq.com>
2019-12-24 12:34:03 +0800

    springboot-02-config

---
+++ springboot-2-config/src/main/java/com/eden/springboot2config/Springboot2ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.eden.springboot2config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2ConfigApplication.class, args);
+    }
+
+}
---
+++ springboot-2-config/src/test/java/com/eden/springboot2config/Springboot2ConfigApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eden.springboot2config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2ConfigApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
